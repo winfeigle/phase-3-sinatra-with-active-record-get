134d82697fe4839271561d746ce8132a796e2569
Win Feigle <win.feigle@gmail.com>
2022-11-21 06:09:05 -0700

    commit tree 7701422a934aab1cc585d2569579462e47e9ff9b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,36 @@
 class ApplicationController < Sinatra::Base
 
+    # Add this line to set the Content-Type header for all responses
+  set :default_content_type, 'application/json'
+
   get '/' do
     { message: "Hello world" }.to_json
   end
 
+  get '/games' do
+     # Get all the games from the database, order them by title name and limit the results to 10
+    games = Game.all.order(:title).limit(10)
+      # Return a JSON response with an array of all the game data
+    games.to_json
+  end
+
+  # Use the :id syntax to create a dynamic route
+  get '/games/:id' do
+      # Look up the game in the database using its ID
+    game = Game.find(params[:id])
+
+      # Send a JSON-formatted response of the game data, including reviews. This only works because our Game model has the correct associations set up.
+    # game.to_json(include: :reviews)
+
+      # Or we could include users associated with each review
+    # game.to_json(include: { reviews: { include: :user } })
+
+      # You can use "only" to limit results
+      game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
+    
+  end
 end
